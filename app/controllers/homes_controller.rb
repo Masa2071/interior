91fdbfcec0797c93780b86fa06f76a99331edb7e
EC2 Default User <ec2-user@ip-172-31-28-56.ap-northeast-1.compute.internal>
2021-03-25 11:08:05 +0000

    update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,13 @@
 class HomesController < ApplicationController
     def top
     end
+    
+    def company
+    end
+    
+    def recruit
+    end
+    
+    def contact
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+   get "homes/company" => "homes#company"
+   get 'homes/recruit' => 'homes#recruit'
+   get 'homes/contact' => 'homes#contact'
 end
